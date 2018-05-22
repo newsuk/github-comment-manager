e31b5dd51683e435da37c35ad0655227e5b7e4ac
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-05-22 12:15:30 +0100

    test: create action full coverage. (#2)

--- src/actions/create.test.js
+++ src/actions/create.test.js
@@ -1,10 +1,12 @@
 import sinon from 'sinon';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import chaiAsPromised from 'chai-as-promised';
 import request from 'request';
 import create from './create';
 
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 
 describe('create', () => {
   let sandbox;
@@ -50,6 +52,34 @@ describe('create', () => {
       );
       postStub.firstCall.args[0].headers['User-Agent'].should.equal(account);
       postStub.firstCall.args[0].body.should.equal(`{"body": "${comment}"}`);
+    });
+
+    it('should reject with an error when a none 2xx status code is received', () => {
+      // Setup.
+      const account = 'tools';
+      const token = 'super-secure-token';
+      const repository = 'github-comment-manager';
+      const comment = 'I am a new comment';
+      const pullRequest = 9000;
+
+      const error = null;
+      const responseBody = 'Not Found';
+      const response = { body: responseBody, statusCode: '404' };
+      sandbox
+        .stub(request, 'post')
+        .callsArgWith(1, error, response, responseBody);
+
+      // Exercise.
+      const createAction = create.comment({
+        account,
+        token,
+        repository,
+        comment,
+        pullRequest
+      });
+
+      // Verify.
+      return createAction.should.have.rejectedWith(responseBody);
     });
   });
 });
